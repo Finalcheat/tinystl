99b4b9eab0d8f5a78e8a2132c6c71dfe49e4bac6
Finalcheat <finalcheat@gmail.com>
2018-05-06 14:21:55 +0800

    stack add list container test

--- test/stack_unittest.cpp
+++ test/stack_unittest.cpp
@@ -3,12 +3,13 @@
 
 #include "gtest/gtest.h"
 #include "stack.hpp"
+#include "list.hpp"
 #define TNS tinystl
 
 
 namespace {
 
-    TEST(stack_test, stack_test_1)
+    TEST(stack_test, stack_default_container_test)
     {
         TNS::stack<int> s;
         ASSERT_TRUE(s.empty());
@@ -16,6 +17,29 @@ namespace {
         s.push(42);
         s.push(101);
         s.push(69);
+
+        ASSERT_EQ(s.size(), 3);
+
+        ASSERT_FALSE(s.empty());
+        ASSERT_EQ(s.top(), 69);
+        s.pop();
+        ASSERT_EQ(s.top(), 101);
+        s.pop();
+        ASSERT_EQ(s.top(), 42);
+        s.pop();
+        ASSERT_TRUE(s.empty());
+    }
+
+    TEST(stack_test, stack_list_container_test)
+    {
+        TNS::stack<int, TNS::list<int> > s;
+        ASSERT_TRUE(s.empty());
+
+        s.push(42);
+        s.push(101);
+        s.push(69);
+
+        ASSERT_EQ(s.size(), 3);
 
         ASSERT_FALSE(s.empty());
         ASSERT_EQ(s.top(), 69);
